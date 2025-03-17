73acdabd537bf29894bc60f87790d7cb025dba19
Rawann0m <raawannm@gmail.com>
2025-03-17 12:51:46 +0300

    Initial Commit

---
+++ APIWeather/APIWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIWeatherApp.swift
+//  APIWeather
+//
+//  Created by Rawan on 17/09/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct APIWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
